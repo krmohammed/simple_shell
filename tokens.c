be32efc779d1e799f66107cb528b079430785745
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-05-22 16:42:45 -0700

    handling betty checker

--- tokens.c
+++ tokens.c
@@ -2,7 +2,7 @@
 
 /**
  * tokens - breaks commands entered into tokens
- * @command: string containing command entered
+ * @commands: string containing command entered
  * @command_len: length of command
  *
  * Return: array of pointers to the tokens
@@ -10,39 +10,37 @@
 
 char **tokens(char *commands, ssize_t command_len)
 {
-        char *token, *commands_cpy = NULL;
-        char **full_command;
-        int n_token, i;
-        char *delim = " \n";
+	char *token, *commands_cpy = NULL;
+	char **full_command;
+	int n_token, i;
+	char *delim = " \n";
 
-        commands_cpy = malloc(sizeof(char) * command_len);
-        if (commands_cpy == NULL)
-        {
-                return (NULL);
-        }
+	commands_cpy = malloc(sizeof(char) * command_len);
+	if (commands_cpy == NULL)
+	{
+		return (NULL);
+	}
 
-        _strcpy(commands_cpy, commands);
-        token = strtok(commands, delim);
+	_strcpy(commands_cpy, commands);
+	token = strtok(commands, delim);
 
-        n_token = 0;
-        while (token != NULL)
-        {
-                n_token++;
-                token = strtok(NULL, delim);
-        }
+	n_token = 0;
+	while (token != NULL)
+	{
+		n_token++;
+		token = strtok(NULL, delim);
+	}
 
-        full_command = malloc(sizeof(char *) * n_token);
-        token = strtok(commands_cpy, delim);
-        for (i = 0; token != NULL; i++)
-        {
-                full_command[i] = malloc(sizeof(char) * _strlen(token));
-                _strcpy(full_command[i], token);
-                token = strtok(NULL, delim);
-        }
-        full_command[i] = NULL;
+	full_command = malloc(sizeof(char *) * n_token);
+	token = strtok(commands_cpy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		full_command[i] = malloc(sizeof(char) * _strlen(token));
+		_strcpy(full_command[i], token);
+		token = strtok(NULL, delim);
+	}
+	full_command[i] = NULL;
 
-        /*execute_command(full_command, argv);*/
-
-        free(commands_cpy);
-        return (full_command);
+	free(commands_cpy);
+	return (full_command);
 }
